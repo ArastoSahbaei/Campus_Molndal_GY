a76991337ac5660d6f6025c417247c8f83141f17
arasto <Arasto.Sahbaei@gmail.com>
2021-02-24 13:20:37 +0100

    m

--- javascript/DataTypes2.js
+++ javascript/DataTypes2.js
@@ -18,6 +18,43 @@ const newPerson = { firstName: "Arasto", isTeacher: true }
 const newPerson2 = { firstName: "randomName", lastName: "randomLastName", age: 500, isTeacher: false }
 const people = [newPerson, newPerson2]
 
+function sortArray() {
+	cars.sort()
+	alert(cars)
+}
+
+function sliceExample() {
+	let x = cars.slice(3)
+	alert(x)
+}
+
+function concatenatingTwoArrays() {
+	const concatenatingTwoArrays = superHeroes.concat(cars)
+	alert(concatenatingTwoArrays)
+}
+
+function removeValueFromArrayWithSplice() {
+	cars.splice(3, 1)
+	alert(cars)
+}
+
+function addValuesToArray() {
+	cars.splice(2, 0, "Bil1", "Bil2")
+	alert(cars)
+}
+
+function removeFirstIndexFromArray() {
+	alert(cars)
+	cars.shift()
+	alert(cars)
+}
+
+function removeFromArray() {
+	alert(cars)
+	cars.pop()
+	alert(cars)
+}
+
 function getObjectValueFromArray() {
 	alert(people[1].firstName)
 }
